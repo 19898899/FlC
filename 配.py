db558c206596206e98b0a902fa8e48ab25ee7047
19898899 <131737173+19898899@users.noreply.github.com>
2025-12-12 00:32:48 +0800

    Update 配.py

--- 配.py
+++ 配.py
@@ -10334,7 +10334,19 @@ rules:
   - PROCESS-NAME,Transmission,🎯 全球直连
   - PROCESS-NAME,uTorrent,🎯 全球直连
   - PROCESS-NAME,WebTorrent,🎯 全球直连
-  - PROCESS-NAME,WebTorrent Helper,🎯 全球直连
+0s
+Run peaceiris/actions-gh-pages@v3
+[INFO] Usage https://github.com/peaceiris/actions-gh-pages#readme
+Dump inputs
+Setup auth token
+Prepare publishing assets
+Setup Git config
+Create a commit
+Push the commit or tag
+  /usr/bin/git push origin gh-pages
+  remote: Permission to 19898899/FlC.git denied to github-actions[bot].
+  fatal: unable to access 'https://github.com/19898899/FlC.git/': The requested URL returned error: 403
+  Error: Action failed with "The process '/usr/bin/git' failed with exit code 128"
   - PROCESS-NAME,DownloadService,🎯 全球直连
   - PROCESS-NAME,Weiyun,🎯 全球直连
   - DOMAIN-KEYWORD,aria2,🎯 全球直连
